a3a945847ff79cdd692f636fce989e9972a03323
Slepoyi <zub.dayu@mail.ru>
2023-05-16 17:38:46 +0300

    Add rectangularity unit tests

---
+++ tests/Domain.UnitTests/Tests/RectangularityTests.cs
@@ -0,0 +1,16 @@
+﻿using Domain.UnitTests.TestData;
+using Geometry.Domain.Shapes;
+
+namespace Domain.UnitTests.Tests
+{
+    public class RectangularityTests
+    {
+
+        [Theory]
+        [ClassData(typeof(RectangularityTestData))]
+        public void TriangleIsRectangular_ShouldReturnCorrectResult_WhenTriangleAndExpectedResultPassed(Triangle t, bool isRectangular)
+        {
+            Assert.Equal(t.IsRectangular, isRectangular);
+        }
+    }
+}
