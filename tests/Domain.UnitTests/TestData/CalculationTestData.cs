bfb8dfcab5af8c4a7769055847071714746eb32d
Slepoyi <zub.dayu@mail.ru>
2023-05-31 20:20:06 +0300

    Add more test data for edge cases

--- tests/Domain.UnitTests/TestData/CalculationTestData.cs
+++ tests/Domain.UnitTests/TestData/CalculationTestData.cs
@@ -11,6 +11,12 @@ namespace Geometry.UnitTests.TestData
             new object[] { new Circle(2), 12.566371 },
             new object[] { new Triangle(5, 4, 3), 6 },
             new object[] { new Triangle(1.5, 2, 1), 0.726184 },
+            new object[] { new Circle(double.MaxValue - 1), -1 },
+            new object[] { new Triangle(
+                (double.MaxValue / 2) - 5,
+                (double.MaxValue / 2) - 4,
+                (double.MaxValue / 2) - 3),
+                -1 }
         };
 
         public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
--- tests/Domain.UnitTests/TestData/ValidationTestData.cs
+++ tests/Domain.UnitTests/TestData/ValidationTestData.cs
@@ -11,11 +11,17 @@ namespace Geometry.UnitTests.TestData
             new object[] { new Circle(1.5), true },
             new object[] { new Circle(-2), false },
             new object[] { new Circle(0), false },
+            new object[] { new Circle(double.MaxValue), true },
             new object[] { new Triangle(5, 4, 3), true },
             new object[] { new Triangle(1.5, 2, 1), true },
             new object[] { new Triangle(-2, 5, 2), false },
             new object[] { new Triangle(0, 1, 1), false },
-            new object[] { new Triangle(100, 4, 3), false }
+            new object[] { new Triangle(100, 4, 3), false },
+             new object[] { new Triangle(
+                (double.MaxValue / 2) - 5,
+                (double.MaxValue / 2) - 4,
+                (double.MaxValue / 2) - 3),
+                true }
         };
 
         public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
